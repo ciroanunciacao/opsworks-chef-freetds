81fb905abbc9573cc3e7294f25f8fdb763d5dedb
Ciro Anunciação <ciro.anunciacao@gmail.com>
2021-09-30 15:23:39 -0300

    Update install.rb

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -12,6 +12,7 @@ node[:deploy].each do |application, deploy|
     php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
     php composer-setup.php
     php -r "unlink('composer-setup.php');"
+    cat "#{deploy[:deploy_to]}/current/composer.json"
     php composer.phar install --no-dev --no-interaction --optimize-autoloader
     EOH
     only_if { ::File.exists?("#{deploy[:deploy_to]}/current/composer.json") }
