df942339da9ecf8206f1f9371714c540a8e5d274
Leandro Faria <leandroffaria@gmail.com>
2021-09-30 23:38:47 -0300

    Update install.rb

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -3,13 +3,13 @@
 # http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
 #
 node[:deploy].each do |application, deploy|
-  script "install_composer" do
+  sscript "install_composer" do
     interpreter "bash"
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    php -r "readfile('https://getcomposer.org/installer');" | php
-    php composer.phar install --no-dev --no-interaction --optimize-autoloader
+    curl -sS https://getcomposer.org/installer | php
+    php composer.phar install --no-dev
     EOH
     only_if { ::File.exists?("#{deploy[:deploy_to]}/current/composer.json") }
   end
