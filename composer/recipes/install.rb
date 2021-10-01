2bd84cd8c0437644149c3fc442a9fd1cf9970098
Leandro Faria <leandroffaria@gmail.com>
2021-10-01 00:08:53 -0300

    Update install.rb

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -8,7 +8,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    curl -sS https://getcomposer.org/installer | php
+    curl -sS https://getcomposer.org/installer --insecure | php
     php composer.phar install --no-dev
     EOH
     only_if { ::File.exists?("#{deploy[:deploy_to]}/current/composer.json") }
