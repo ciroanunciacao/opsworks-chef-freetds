ee4dc2f5c6fbbf6dfcf4f27210f804a466ba6d1c
Leandro Faria <leandroffaria@gmail.com>
2021-09-30 23:26:32 -0300

    rollback: to commit ca8e1d80fa506e7c74d928a990dc9e5a00436175
    https://github.com/ciroanunciacao/opsworks-chef-freetds/commit/ca8e1d80fa506e7c74d928a990dc9e5a00436175#diff-1236d1aaf0be19be375a6430a047b7fa4a9358689cec39da2e847e56760a6498

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -8,14 +8,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-    php composer-setup.php
-    php -r "unlink('composer-setup.php');"
-    unset HTTP_PROXY
-    unset http_proxy
-    unset HTTPS_PROXY
-    unset https_proxy
+    curl -s https://getcomposer.org/installer | php
     php composer.phar install --no-dev --no-interaction --optimize-autoloader
     EOH
     only_if { ::File.exists?("#{deploy[:deploy_to]}/current/composer.json") }
